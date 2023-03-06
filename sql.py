363afd79f968a3b3f0aa3e5d987711d2ccf74df1
yacine717 <yacine7.hechmi7@gmail.com>
2023-03-06 23:36:18 +0100

    added requirements file

--- sql.py
+++ sql.py
@@ -7,11 +7,10 @@ logger = logging.getLogger(__name__)
 # set up the connection for given database name 'db' to a postgresql server
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
-
 def do_upsert(db, records=None):
     try:
         conn = psycopg2.connect(
-            host='localhost',
+            host='172.18.0.2',
             database=db,
             user='postgres',
             password='postgres',
@@ -25,9 +24,11 @@ def do_upsert(db, records=None):
             #  and use that id to proceed inserting to the other related tables
             cur.execute(upsert_into_tables[0], records[0])
             business_id = cur.fetchone()[0]
+            #  append the business_id to the rest of queries data
             records = [item+(business_id,) for item in records[1:]]
             records_index = 0
             for statement in upsert_into_tables[1:]:
+                #  loop through the rest of statements and execute them
                 try:
                     cur.execute(statement, records[records_index])
                     records_index += 1
