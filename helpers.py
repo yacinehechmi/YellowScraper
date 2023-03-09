01f09d836dbf1a33dd8f324c349fce7ce3686be2
yacine717 <yacine7.hechmi7@gmail.com>
2023-03-09 06:03:26 +0100

    fixed key checking in find_nearby_cities

--- helpers.py
+++ helpers.py
@@ -4,5 +4,5 @@ def find_nearby_cities(nearby_cities, cities):
     if nearby_cities is not None:
         for nearby_city in nearby_cities:
             for name in nearby_city.find_all('a', {'href': True}):
-                if name['href'] not in [item.keys for item in cities]:
+                if name['href'] not in [item for subdict in cities for item in subdict]:
                     cities.append({name['href']: False})
