29e2527288fab1aab6144bb4ef0fee8a7ea122b2
yacine717 <yacine7.hechmi7@gmail.com>
2023-03-09 23:39:40 +0100

    added returned bool, to check if main function need to rerun it

--- helpers.py
+++ helpers.py
@@ -6,3 +6,6 @@ def find_nearby_cities(nearby_cities, cities):
             for name in nearby_city.find_all('a', {'href': True}):
                 if name['href'] not in [item for subdict in cities for item in subdict]:
                     cities.append({name['href']: False})
+        return True
+    else:
+        return False
