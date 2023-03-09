450d357fc8bfb07b35ac0216f7eee00b205a8f96
yacine717 <yacine7.hechmi7@gmail.com>
2023-03-09 03:03:29 +0100

    removed soup logic from fetch_page function

--- scraper.py
+++ scraper.py
@@ -8,17 +8,87 @@ from fake_useragent import UserAgent
 # project mudules
 from classBusiness import Business
 from helpers import find_nearby_cities
-from sql import do_upsert, create_db_and_tables
+from sql import do_upsert
 from process_to_csv import write_to_csv, clean_csv
-from parameters import config, upsert_into_tables
+from parameters import config
 
-
+db = config['db']
 cities = config['cities']
 number_of_pages = config['number_of_pages']
-db = config['db']
 csv_file_name = config['csv_file_name']
 
 
+def scrape_clean_store(soup, db):
+    # get all elements with tag: div and class: info
+    # card = soup.find_all('div', _class="v-card")
+    start = time.time()
+    # print(soup.find_all('div', {"class": "info"}))
+    card = soup.find_all('div', {"class": "info"})
+
+    # loop through all info div's of a single page found by the soup
+    for i, j in enumerate(card):
+        # extantiate class business and assign soup content to class attributes
+        business = Business(
+            j.find('a', {'class': 'business-name'}),  # name
+            j.find('div', {'class': 'phone'}),  # phone
+            j.find('div', {'class': 'locality'}),  # locality
+            j.find('div', {'class': 'price-range'}),  # price_range
+            j.find('div', {'class': 'open-status'}),  # open_status
+            j.find('div', {'class': 'result-rating'}),  # rating
+            j.find('div', {'class': 'count'}),  # rating_count
+            j.find('div', {'class': 'ratings'}),  # tripadvisor
+            j.find('div', {'class': 'ratings'}),  # foursquare_rating
+            j.find_all("div", {"class": "categories"}),  # categories
+            j.find_all("div", {"class": "amenities-info"}),  # amenities
+            j.find('a', {'class': 'track-visit-website', 'href': True}),  # website
+            j.find('a', {'class': 'order-online', 'href': True}),  # order_online
+            j.find('div', {'class': 'number'}), #year_in_business
+        )
+        # values to be stored in business_info table
+        business_info = (
+                business.get_name(),
+                business.get_phone(),
+                business.get_price_range(),
+                business.get_year_in_business(),
+                business.get_amenities(),
+                business.get_categories(),
+                business.city,
+                business.state_code,
+                business.zip_code,
+        )
+        # values to be stored in access_info table
+        access_info = (
+            business.get_open_status(),
+            business.get_website(),
+            business.get_order(),
+        )
+        # values to be stored in yellowpages_info table
+        yellowpages_info = (
+            business.get_rating(),
+            business.get_rating_count(),
+        )
+        # values to be stored in tripadvisor_info table
+        tripadvisor_info = (
+            business.get_tripadvisor_rating(),
+            business.get_tripadvisor_rating_count(),
+        )
+        # values to be stored in foursquare_info table
+        foursquare_info = (
+            business.get_foursquare_rating(),
+        )
+
+        records = [
+            business_info,
+            access_info,
+            yellowpages_info,
+            tripadvisor_info,
+            foursquare_info
+        ]
+        # connect to { db } & upsert { records } into tables business_info,
+        # access_info, yellowpages_info, tripadvisor_info, foursquare_info
+
+        do_upsert(db, records)
+        print(f'{i} time consumed : {time.time() - start}')
 def fetch_page(num_of_page, city):
     try:
         ua = UserAgent()
