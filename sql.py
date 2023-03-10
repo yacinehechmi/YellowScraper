44cffc1e580d911ad544c91283e417f3c0808514
yacine717 <yacine7.hechmi7@gmail.com>
2023-03-10 01:20:08 +0100

    added .env variables, fixed whitespace errors

--- sql.py
+++ sql.py
@@ -1,28 +1,29 @@
 import psycopg2
 from dotenv import load_dotenv
 import os
-from parameters import create_queries, upsert_into_tables
 import logging
+# project modules
+from parameters import create_queries, upsert_into_tables
+
 logger = logging.getLogger(__name__)
-# set up the connection for given database name 'db' to a postgresql server
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 
 def do_upsert(db, records=None):
     try:
         conn = psycopg2.connect(
-            host='172.18.0.2',
+            host=os.getenv('HOST'),
             database=db,
-            user='postgres',
-            password='postgres',
-            port='5432',
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            port=os.getenv('HOST'),
         )
         conn.autocommit = True
         logger.info('DATABASE CONNECTION SUCCESSFUL')
         cur = conn.cursor()
+        #  run first query to insert business_info data and get back the id of a record
+        #  and use that id to proceed inserting to the other related tables
         try:
-            #  run first query to insert business_info data and get back the id of a record
-            #  and use that id to proceed inserting to the other related tables
             cur.execute(upsert_into_tables[0], records[0])
             business_id = cur.fetchone()[0]
             #  append the business_id to the rest of queries data
@@ -44,15 +45,16 @@ def do_upsert(db, records=None):
         print(error)
         logger.error(f'DATABASE CONNECTION FAILED: {error}')
 
-# even better
+
+# Create tables business_info, access_info, yellowpages_info, tripadvisor_info, foursquare_info
 def create_tables(db):
     try:
         conn = psycopg2.connect(
-            host='localhost',
+            host=os.getenv('HOST'),
             database=db,
-            user='postgres',
-            password='postgres',
-            port='5432',
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            port=os.getenv('HOST'),
         )
         conn.autocommit = True
         cur = conn.cursor()
@@ -60,28 +62,26 @@ def create_tables(db):
             cur.execute(create_queries["create_tables"])
         except (Exception, psycopg2.DatabaseError) as error:
             logger.error(f'DATABASE QUERY FAILED: {create_queries["create_tables"]} \n error: {error}')
-            print(error)
         conn.close()
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
         logger.error(f'DATABASE CONNECTION FAILED : {error}')
 
 
+# Create database in postgresql server
 def create_db():
     try:
         conn = psycopg2.connect(
-            host='localhost',
-            database="",
-            user='postgres',
-            password='postgres',
-            port='5432',
+            host=os.getenv('HOST'),
+            database=os.getenv('DB'),
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            port=os.getenv('HOST'),
         )
         conn.autocommit = True
         cur = conn.cursor()
         try:
             cur.execute(create_queries["create_db"])
-            print(create_queries["create_db"])
-            logger.info(f'DATABASE QUERY SUCCESSFUL: {create_queries["create_db"]}')
         except (Exception, psycopg2.DatabaseError) as error:
             logger.error(f'DATABASE QUERY FAILED: {create_queries["create_db"]} \n error: {error}')
             print(error)
