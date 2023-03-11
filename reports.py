83f551725c100822d2d92126353a33123c43abf6
yacine717 <yacine7.hechmi7@gmail.com>
2023-03-11 06:16:21 +0100

    added sample csv data

--- reports.py
+++ reports.py
@@ -1,14 +1,13 @@
-from sql import select_join
 from sqlalchemy import create_engine
-
 from Settings import settings, queries
+import pandas as pd
 
 host = settings['db_creds']['host']
 database = 'yellowpages'
 user = settings['db_creds']['user']
 password = settings['db_creds']['pass']
 port = settings['db_creds']['port']
-
+db = settings['db_creds']['db']
 #  what i need to do:
 # create dataframe
 # use dataframe to create csv
@@ -16,10 +15,14 @@ port = settings['db_creds']['port']
 
 
 def main():
-    engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/yellowpages')
+    engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}')
+    # add an empty dict here
+    # add empty dataframe
+    result_proxy = engine.execute(queries['select'])
+    results = result_proxy.fetchall()
+    df = pd.DataFrame(results, columns=result_proxy.keys())
+    df.to_csv('data/sample_data.csv')
+    print(df.info())
 
-    result = engine.execute(queries['select'])
-    for record in result:
-        print(record)
 if __name__ == "__main__":
     main()
