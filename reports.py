fcf8f472e5f257dc35482142f073609fd0f1e419
yacine717 <yacine7.hechmi7@gmail.com>
2023-03-14 14:47:09 +0100

    fixed whitespaces, ordered imports

--- reports.py
+++ reports.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
-from Settings import settings, queries
 import pandas as pd
+
+from sql.sql import queries
+from settings import settings
 
 host = settings['db_creds']['host']
 database = 'yellowpages'
@@ -8,10 +10,6 @@ user = settings['db_creds']['user']
 password = settings['db_creds']['pass']
 port = settings['db_creds']['port']
 db = settings['db_creds']['db']
-#  what i need to do:
-# create dataframe
-# use dataframe to create csv
-# use dataframe to create visualizations
 
 
 def main():
@@ -23,6 +21,7 @@ def main():
     df = pd.DataFrame(results, columns=result_proxy.keys())
     df.to_csv('data/sample_data.csv')
     print(df.info())
+
 
 if __name__ == "__main__":
     main()
