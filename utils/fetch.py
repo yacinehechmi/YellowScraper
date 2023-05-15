2d069253ca76c3a11574ab65e03fa5d0e6f91016
yacinehechmi <yacine7.hechmi7@gmail.com>
2023-05-15 06:21:27 +0100

    major changes, added async fetching modules, removed the old ones

--- utils/fetch.py
+++ utils/fetch.py
@@ -1,29 +1,42 @@
+import aiohttp
+import asyncio
 from fake_useragent import UserAgent
 
-import requests
-import logging
 
-logger = logging.getLogger(__name__)
+async def fetch_page(session, url):
+    async with session.get(url) as res:
+        return await res.text()
 
 
-def fetch_page(num_of_page, city):
-    print(num_of_page, city)
-    try:
-        # randomize header's user_agent
-        ua = UserAgent()
-        user_agent = ua.random
-        headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,"
-                      "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Connection": "keep-alive",
-            "User-Agent": user_agent,
-            "Cache-Control": "max-age=0, no-cache, no-store",
-            "Upgrade-Insecure-Requests": "1"
-        }
-        print('got it')
-        return requests.get(f"https://www.yellowpages.com{city}", headers=headers, params={
-                "page": num_of_page})
-    except requests.HTTPError as errHTTP:
-        logger.error(f" GOT {errHTTP} AT: num_of_page:{num_of_page} city:{city}")
+async def fetch_all(session, cities):
+    tasks = []
+    # put the loops here, when looping through cities and num of pages
+    print(type(cities))
+    for city in cities:
+        for page in range(30):
+            if page > 1:
+                url = f'https://www.yellowpages.com{city}/?page={page}'
+            else:
+                url = f'https://www.yellowpages.com{city}/'
+            task = asyncio.create_task(fetch_page(session, url))
+            tasks.append(task)
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+async def fetch(cities):
+    ua = UserAgent()
+    user_agent = ua.random
+    headers = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,"
+                "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Connection": "keep-alive",
+                "User-Agent": user_agent,
+                "Cache-Control": "max-age=0, no-cache, no-store",
+                "Upgrade-Insecure-Requests": "1"
+                }
+    async with aiohttp.ClientSession(headers = headers) as session:
+        data = await fetch_all(session, cities)
+        return data
