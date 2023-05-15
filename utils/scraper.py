d7adee068a23da3e3467f47524312e0a49a1990a
yacinehechmi <yacine7.hechmi7@gmail.com>
2023-05-15 06:19:11 +0100

    added pagination, build_cities_list modules, changed find_nearby_cities

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,16 +1,30 @@
+from bs4 import BeautifulSoup as bs
 def find_pagination(pagination):
     if pagination is not None:
         print(pagination.find('span'))
+        pag_str = pagination.find('span')
+        x = slice(pag_str.find('-')+1, pag_str.rfind('o')-1)
+        y = slice(pag_str.rfind(' ')+1, len(pag_str))
+        return int(int(pag_str[y])/int(pag_str[x]))
     else:
-        print('no pagination')
+        return None
 
 
 def find_nearby_cities(nearby_cities, cities):
     if nearby_cities is not None:
         for nearby_city in nearby_cities:
             for name in nearby_city.find_all('a', {'href': True}):
-                if name['href'] not in [item for subdict in cities for item in subdict]:
-                    cities.append({name['href']: False})
-        return True
+                if name['href'] not in [city for city in cities]:
+                    cities.append(name['href'])
     else:
-        return False
+        return None
+
+
+def build_cities_list(results, cities):
+    for page in results:
+        soup = bs(page)
+        print(cities)
+        print('---')
+        find_nearby_cities(soup.find('section', {'class': 'nearby-cities'}), cities)
+    return cities
+        
