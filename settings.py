a0fc3bdbd47b5b2e1b74524ab473b9e80bab3117
yacinehechmi <yacine7.hechmi7@gmail.com>
2023-05-16 03:56:49 +0100

    replaced dict with array and renamed keys

--- settings.py
+++ settings.py
@@ -4,14 +4,14 @@ import os
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 settings = {
-    "csv_file_name": 'busi.csv',
-    "number_of_pages": 2,
-    "cities": [{'/los-angeles-ca/retaurants': False}],
-    "db_creds": {
-        "host": os.getenv('HOST'),
-        "port": os.getenv('PORT'),
-        "pass": os.getenv('PG_PASS'),
-        "user": os.getenv('PG_USER'),
-        "db": os.getenv('DB'),
+    'csv_file_name': 'business.csv',
+    'pagination': 3,
+    'cities': ['/los-angeles-ca/retaurants'],
+    'db_creds': {
+        'host': os.getenv('HOST'),
+        'port': os.getenv('PORT'),
+        'pass': os.getenv('PG_PASS'),
+        'user': os.getenv('PG_USER'),
+        'db': os.getenv('DB'),
     },
 }
