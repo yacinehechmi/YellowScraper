3fc0f643ae6a7c80991e8309211fe43c8c339b10
yacinehechmi <yacine7.hechmi7@gmail.com>
2023-05-21 15:19:34 +0100

    fixed headers

--- utils/fetch.py
+++ utils/fetch.py
@@ -8,16 +8,16 @@ async def fetch_page(session, url):
         ua = UserAgent()
         user_agent = ua.random
         headers = {
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,"
-                "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Connection": "keep-alive",
-                "User-Agent": user_agent,
-                "Cache-Control": "max-age=0, no-cache, no-store",
-                "Upgrade-Insecure-Requests": "1"
+                'Accept': '''text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,'''
+                '''image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9''',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Connection': 'keep-alive',
+                'User-Agent': user_agent,
+                'Cache-Control': 'max-age=0, no-cache, no-store',
+                'Upgrade-Insecure-Requests': '1'
                 }
-        async with session.get(url, headers = headers) as res:
+        async with session.get(url, headers=headers) as res:
             return await res.text()
     except aiohttp.ClientConnectorError as e:
         print('connection failed', str(e))
@@ -42,11 +42,9 @@ async def fetch_all(session, cities, pagination):
     res = None
     while not res:
         res = await asyncio.gather(*tasks)
-    cities = {city: True for city,is_scraped in cities.items()}
     return res
 
 
 async def fetch(cities, pagination):
     async with aiohttp.ClientSession() as session:
         return await fetch_all(session, cities, pagination)
-        
