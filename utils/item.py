f424e9b03917ec22ce431bff6aedd52010c03223
yacinehechmi <yacine7.hechmi7@gmail.com>
2023-05-21 10:38:42 +0100

    added methods to return each table content

--- utils/item.py
+++ utils/item.py
@@ -36,9 +36,12 @@ class Business:
         self.website = website
         if self.locality is not None:
             self.city = self.locality.text[0:self.locality.text.find(',')]
-            self.zip_code = self.locality.text[self.locality.text.rindex(' ')+1:]
-            self.state_code = self.locality.text[self.locality.text.find(',') + 2:
-                                                 len(self.locality.text) - len(self.zip_code)-1]
+            self.zip_code = \
+                self.locality.text[self.locality.text.rindex(' ')+1:]
+            self.state_code = \
+                self.locality.text[self.locality.text.find(',') + 2:
+                                   len(self.locality.text)
+                                   - len(self.zip_code)-1]
         else:
             self.city = None
             self.zip_code = None
@@ -46,15 +49,13 @@ class Business:
         self.order_online = order_online
         self.year_in_business = year_in_business
 
-
     def get_name(self):
         if self.name is None:
             return None
         else:
             # replace characters to not break the sql query
-            # pretty sure there is a better way to handle this 
+            # pretty sure there is a better way to handle this
             return self.name.text.replace("'", "_", 1)
-
 
     def get_phone(self):
         if self.phone is None:
@@ -62,20 +63,17 @@ class Business:
         else:
             return self.phone.text
 
-
     def get_locality(self):
         if self.locality is None:
             return None
         else:
             return self.locality.text
 
-
     def get_price_range(self):
         if self.price_range is None:
             return None
         else:
             return len(self.price_range.text)
-
 
     def get_open_status(self):
         if self.open_status is None:
@@ -104,13 +102,11 @@ class Business:
                         rate_sum += x
             return rate_sum
 
-
     def get_rating_count(self):
         if self.rating_count is None:
             return None
         else:
             return int(self.rating_count.text)
-
 
     def get_tripadvisor_rating(self):
         try:
@@ -119,7 +115,6 @@ class Business:
         except (TypeError, KeyError):
             return None
 
-
     def get_tripadvisor_rating_count(self):
         try:
             dict_tripadvisor = json.loads(self.tripadvisor['data-tripadvisor'])
@@ -127,13 +122,11 @@ class Business:
         except (TypeError, KeyError):
             return None
 
-
     def get_foursquare_rating(self):
         try:
             return float(self.foursquare_rating['data-foursquare'])
         except (TypeError, KeyError):
             return None
-
 
     def get_amenities(self):
         if self.amenities is None or self.amenities == []:
@@ -143,15 +136,14 @@ class Business:
                               for amenity in amenities_list.find_all('span')]
             return amenities_list
 
-
     def get_categories(self):
         if self.categories is None or self.categories == []:
             return None
         else:
-            categories_list = [category.text for categories_list in self.categories
+            categories_list = [category.text for categories_list
+                               in self.categories
                                for category in categories_list.find_all('a')]
         return categories_list
-
 
     def get_website(self):
         if self.website is None:
@@ -159,16 +151,51 @@ class Business:
         else:
             return self.website['href']
 
-
     def get_order(self):
         if self.order_online is None:
             return None
         else:
             return self.order_online.text
 
-
     def get_year_in_business(self):
         if self.year_in_business is None:
             return None
         else:
             return self.year_in_business.text
+
+    def get_tripadvisor_info(self):
+        return (
+                self.get_tripadvisor_rating(),
+                self.get_tripadvisor_rating_count()
+                )
+
+    def get_foursquare_info(self):
+        return (
+                self.get_foursquare_rating(),
+                )
+
+    def get_yellowpages_info(self):
+        return (
+                self.get_rating(),
+                self.get_rating_count()
+                )
+
+    def get_access_info(self):
+        return (
+                self.get_open_status(),
+                self.get_website(),
+                self.get_order()
+                )
+
+    def get_business_info(self):
+        return (
+                self.get_name(),
+                self.get_phone(),
+                self.get_price_range(),
+                self.get_year_in_business(),
+                self.get_amenities(),
+                self.get_categories(),
+                self.city,
+                self.state_code,
+                self.zip_code,
+                )
