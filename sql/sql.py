f4a5f964795252a2df4240a602f1ff46093c5712
yacinehechmi <yacine7.hechmi7@gmail.com>
2023-05-21 15:18:57 +0100

    removed pointless logging

--- sql/sql.py
+++ sql/sql.py
@@ -19,7 +19,7 @@ def get_conn(db=None):
             port=settings['db_creds']['port'],
         )
         return conn
-    except(Exception, psycopg2.DatabaseError) as error:
+    except (Exception, psycopg2.DatabaseError) as error:
         logger.error(f'DATABASE CONNECTION FAILED: {error}')
 
 
@@ -28,32 +28,35 @@ def select_join(db):
     try:
         df = pd.read_sql(conn, queries['select'])
     except (Exception, psycopg2.DatabaseError) as error:
-        logger.error(f'DATABASE QUERY FAILED: {queries["upsert_into_tables"][0]} \n error: {error}')
+        logger.error(f'''DATABASE QUERY FAILED:
+                     {queries['upsert_into_tables'][0]} \n error: {error}''')
     conn.close()
 
 
 def do_upsert(db, records=None):
     conn = get_conn(db)
     conn.autocommit = True
-    logger.info('DATABASE CONNECTION SUCCESSFUL')
     cur = conn.cursor()
-    #  run first query to insert business_info data and get back the id of a record
+    #  run first query to insert business_info data
+    # and get back the id of a record
     #  and use that id to proceed inserting to the other related tables
     try:
-        cur.execute(queries["upsert_into_tables"][0], records[0])
+        cur.execute(queries['upsert_into_tables'][0], records[0])
         business_id = cur.fetchone()[0]
         #  append the business_id to the rest of queries data
         records = [item+(business_id,) for item in records[1:]]
         records_index = 0
-        for statement in queries["upsert_into_tables"][1:]:
+        for statement in queries['upsert_into_tables'][1:]:
             #  loop through the rest of statements and execute them
             try:
                 cur.execute(statement, records[records_index])
                 records_index += 1
             except (Exception, psycopg2.DatabaseError) as error:
-                logger.error(f'DATABASE QUERY FAILED: {statement} \n error: {error}')
+                logger.error(f'''DATABASE QUERY FAILED:
+                             {statement} \n error: {error}''')
     except (Exception, psycopg2.DatabaseError) as error:
-        logger.error(f'DATABASE QUERY FAILED: {queries["upsert_into_tables"][0]} \n error: {error}')
+        logger.error(f'''DATABASE QUERY FAILED:
+                     {queries['upsert_into_tables'][0]} \n error: {error}''')
     conn.close()
 
 
@@ -63,19 +66,22 @@ def create_db():
     conn.autocommit = True
     cur = conn.cursor()
     try:
-        cur.execute(queries["create_db_and_tables"][0])
+        cur.execute(queries['create_db_and_tables'][0])
     except (Exception, psycopg2.DatabaseError) as error:
-        logger.error(f'DATABASE QUERY FAILED: {queries["create_db_and_tables"][0]} \n error: {error}')
+        logger.error(f'''DATABASE QUERY FAILED:
+                     {queries['create_db_and_tables'][0]} \n error: {error}''')
     conn.close()
 
 
-# Create tables business_info, access_info, yellowpages_info, tripadvisor_info, foursquare_info
+# Create tables business_info, access_info, yellowpages_info,
+# tripadvisor_info, foursquare_info
 def create_tables(db):
     conn = get_conn(db)
     conn.autocommit = True
     cur = conn.cursor()
     try:
-        cur.execute(queries["create_db_and_tables"][1])
+        cur.execute(queries['create_db_and_tables'][1])
     except (Exception, psycopg2.DatabaseError) as error:
-        logger.error(f'DATABASE QUERY FAILED: {queries["create_db_and_tables"][1]} \n error: {error}')
+        logger.error(f'''DATABASE QUERY FAILED
+                     {queries['create_db_and_tables'][1]} \n error: {error}''')
     conn.close()
