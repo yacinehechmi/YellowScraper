3a5b35ad47963e4e22029e2f2c3fc20979fa3410
yacinehechmi <yacine7.hechmi7@gmail.com>
2023-06-05 16:30:02 +0100

    removed logging, added settings variables

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-import logging
 import asyncio
 from utils.Parse import Parse
 from utils.Fetch import Fetch
 from utils.dataclasses import Results, Result
-from settings import settings
+from settings import DEFAULT_CITIES, DEFAULT_PAGINATION
 
 
 def main():
-    [Results.updateEndpoints(Result(city, 0, 0, 3))
-     for city in settings['defualt_cities']]
+    [Results.updateEndpoints(Result(city, 0, 0, DEFAULT_PAGINATION))
+     for city in DEFAULT_CITIES]
     while True:
         new_endpoints = Results.getEndpoints()
         if new_endpoints:
             fetchResults = Fetch(new_endpoints)
             results = asyncio.run(fetchResults.fetch())
             Parse(results).parseContentAndStore()
-            
         else:
             False
 
-    print('end')
-
 
 if __name__ == "__main__":
-    logger = logging
-    logger.basicConfig(level=logging.ERROR, filename='logs/scraper.log',
-                       format='[%(asctime)s] %(levelname)s:%(message)s')
     main()
