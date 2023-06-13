310b392a5c2ac59ab76c0b919da853d630c0adb8
yacinehechmi <yacine7.hechmi7@gnail.com>
2023-06-13 17:22:13 +0100

    replaced while by recusive

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from utils.dataclasses import Results, Result
 from settings import DEFAULT_CITIES, DEFAULT_PAGINATION
 
 
-def main():
-    [Results.updateEndpoints(Result(city, 0, 0, DEFAULT_PAGINATION))
-     for city in DEFAULT_CITIES]
-    while True:
-        new_endpoints = Results.getEndpoints()
-        if new_endpoints:
-            fetchResults = Fetch(new_endpoints)
-            results = asyncio.run(fetchResults.fetch())
-            Parse(results).parseContentAndStore()
-        else:
-            False
+def main(new_results: list = None) -> None:
+    if new_results is None:
+        exit()
+    else:
+        results = asyncio.run(Fetch().fetch(new_results))
+        list(map(lambda result: Parse().parse_content_and_store(result)
+                 if result else None,
+                 results))
+        main(Results.get_results())
 
 
 if __name__ == "__main__":
-    main()
+    [Results.set_results(Result(city, 0, DEFAULT_PAGINATION))
+     for city in DEFAULT_CITIES]
+    main(Results.get_results())
