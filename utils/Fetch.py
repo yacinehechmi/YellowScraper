c3a667bda1b82875f7f6e515b6d383e1317972e0
yacinehechmi <yacine7.hechmi7@gnail.com>
2023-06-13 17:24:29 +0100

    removed class attr and set that as a method arg

--- utils/Fetch.py
+++ utils/Fetch.py
@@ -1,15 +1,14 @@
 import asyncio
-from aiolimiter import AsyncLimiter
 import httpx
+from aiolimiter import AsyncLimiter
 from fake_useragent import UserAgent
 from utils.logger import setup_logger
 
-fetch_logger = setup_logger('fetch_logger', 'logs/fetch.log')
+fetch_logger = setup_logger("fetch_logger", "logs/fetch.log")
 
 
-class Fetch:
-    def __init__(self, requested):
-        self.requested = requested
+class Fetch():
+    def __init__(self):
         self.tasks = []
         self.res = None
     # fetching per page
@@ -21,14 +20,14 @@ class Fetch:
                 ua = UserAgent()
                 user_agent = ua.random
                 headers = {
-                        'Accept': '''text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,'''
-                        '''image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9''',
-                        'Accept-Encoding': 'gzip, deflate, br',
-                        'Accept-Language': 'en-US,en;q=0.9',
-                        'Connection': 'keep-alive',
-                        'User-Agent': user_agent,
-                        'Cache-Control': 'max-age=0, no-cache, no-store',
-                        'Upgrade-Insecure-Requests': '1'
+                        "Accept": """text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,"""
+                        """image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9""",
+                        "Accept-Encoding": "gzip, deflate, br",
+                        "Accept-Language": "en-US,en;q=0.9",
+                        "Connection": "keep-alive",
+                        "User-Agent": user_agent,
+                        "Cache-Control": "max-age=0, no-cache, no-store",
+                        "Upgrade-Insecure-Requests": "1"
                         }
                 response = await client.get(url, headers=headers)
                 return response
@@ -45,32 +44,32 @@ class Fetch:
             except (Exception, httpx.TimeoutException) as e:
                 fetch_logger.error(e)
 
-    async def fetch_all(self, session):
-        '''
+    async def fetch_all(self, session, requested):
+        """
         loop through each page in each city and make an async request
         then gather those requests to be executed concurrently
-        '''
+        """
         limiter = AsyncLimiter(100, 0.1)
         async with httpx.AsyncClient() as client:
-            for req in self.requested:
+            for req in requested:
                 for page in range(1, req.total_pages):
                     if page == 1:
-                        url = f'https://www.yellowpages.com{req.endpoint}'
+                        url = f"https://www.yellowpages.com{req.endpoint}"
                     else:
-                        url = f'https://www.yellowpages.com{req.endpoint}/?page={page}'
+                        url = f"""https://www.yellowpages.com{req.endpoint}?page={page}"""
                     self.tasks.append(self.fetch_page(
                         client,
                         url,
                         limiter
                         ))
-            '''
+            """
             added this while loop to make sure we get the requests
             pretty sure there is a better way to do this also
-            '''
+            """
             while not self.res:
                 self.res = await asyncio.gather(*self.tasks)
             return self.res
 
-    async def fetch(self):
+    async def fetch(self, requested):
         async with httpx.AsyncClient() as session:
-            return await self.fetch_all(session)
+            return await self.fetch_all(session, requested)
